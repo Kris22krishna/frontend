997f9e7c9ca7d6192556b757255c2a70ae82e7bd
kris22krishna <kris22krishna@gmail.com>
2026-02-08 11:13:55 +0530

    button name change

--- src/components/home/HeroRegistration.jsx
+++ src/components/home/HeroRegistration.jsx
@@ -8,7 +8,7 @@ const HeroRegistration = ({ selectedRole, setSelectedRole }) => {
         <div className="hero-right">
             <div className="hero-cta-buttons">
                 <button className="btn-primary-large">Start Practicing</button>
-                <button className="btn-secondary-large">Take Skill Assessment</button>
+                <button className="btn-secondary-large">Discover Your Skills</button>
             </div>
         </div>
     );
