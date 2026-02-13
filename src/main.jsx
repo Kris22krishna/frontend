8bcd5a8e7b8906dc23f62dd49640c57a98028920
pranav-p-pathak <patakpranav8423@gmail.com>
2026-02-13 11:25:54 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
+import { Toaster } from 'react-hot-toast';
 import App from './App.jsx';
 import 'katex/dist/katex.min.css';
 import './index.css';
@@ -9,6 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
       <App />
+      <Toaster />
     </HelmetProvider>
   </React.StrictMode>,
 );
