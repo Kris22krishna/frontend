b4c2314baca7ca40040376c39fb6bd0ec233d96c
kris22krishna <kris22krishna@gmail.com>
2026-02-21 13:07:13 +0530

    internship on the navbar

--- src/pages/RapidMathPage.jsx
+++ src/pages/RapidMathPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { SpeedTestGame } from '../components/rapid-math/SpeedTestGame';
 import SEO from '../components/common/SEO';
+import Navbar from '../components/Navbar';
 
 const RapidMathPage = () => {
     return (
@@ -9,6 +10,7 @@ const RapidMathPage = () => {
                 title="Rapid Math - Speed Challenge"
                 description="Test your mathematical speed and accuracy in the Rapid Math challenge."
             />
+            <Navbar />
             <main className="flex-1 flex flex-col">
                 <SpeedTestGame />
             </main>
