193d1642c2874e031ecfa6e8196e316b57b2ff4e
Sathwik Dixith <dixithsathwik.gmail.com>
2026-03-10 19:18:51 +0530

    linear-eq-added

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,12 @@ import Algebra from './components/Math-Branches/Algebra/Algebra';
 import AlgebraIntro5W1H from './components/Math-Branches/Algebra/Topics/5W1H/AlgebraIntro5W1H';
 import AlgebraTerminology from './components/Math-Branches/Algebra/Topics/Terminology/AlgebraTerminology';
 import AlgebraSkills from './components/Math-Branches/Algebra/Topics/Skills/AlgebraSkills';
+
+import LinearEquations from './components/practice/class-8/linear_equations_in_one_variable/LinearEquations';
+import LinearEquationsIntro5W1H from './components/practice/class-8/linear_equations_in_one_variable/Topics/5W1H/LinearEquationsIntro5W1H';
+import LinearEquationsTerminology from './components/practice/class-8/linear_equations_in_one_variable/Topics/Terminology/LinearEquationsTerminology';
+import LinearEquationsSkills from './components/practice/class-8/linear_equations_in_one_variable/Topics/Skills/LinearEquationsSkills';
+
 import IntroductionToGraphs from './components/practice/class-8/introduction_to_graphs/IntroductionToGraphs';
 import GraphsIntro5W1H from './components/practice/class-8/introduction_to_graphs/Topics/5W1H/GraphsIntro5W1H';
 import GraphsTerminology from './components/practice/class-8/introduction_to_graphs/Topics/Terminology/GraphsTerminology';
@@ -525,6 +531,13 @@ function App() {
           <Route path="algebra/introduction" element={<ProtectedRoute redirectTo="/login"><AlgebraIntro5W1H onBack={() => window.history.back()} /></ProtectedRoute>} />
           <Route path="algebra/terminology" element={<ProtectedRoute redirectTo="/login"><AlgebraTerminology onBack={() => window.history.back()} /></ProtectedRoute>} />
           <Route path="algebra/skills" element={<ProtectedRoute redirectTo="/login"><AlgebraSkills onBack={() => window.history.back()} /></ProtectedRoute>} />
+
+          {/* Grade 8: Linear Equations Chapter Pages */}
+          <Route path="senior/grade/8/linear-equations" element={<ProtectedRoute redirectTo="/login"><LinearEquations /></ProtectedRoute>} />
+          <Route path="senior/grade/8/linear-equations/introduction" element={<ProtectedRoute redirectTo="/login"><LinearEquationsIntro5W1H /></ProtectedRoute>} />
+          <Route path="senior/grade/8/linear-equations/terminology" element={<ProtectedRoute redirectTo="/login"><LinearEquationsTerminology /></ProtectedRoute>} />
+          <Route path="senior/grade/8/linear-equations/skills" element={<ProtectedRoute redirectTo="/login"><LinearEquationsSkills /></ProtectedRoute>} />
+
           {/* Grade 8: Introduction to Graphs Chapter Pages */}
           <Route path="senior/grade/8/introduction-to-graphs" element={<ProtectedRoute redirectTo="/login"><IntroductionToGraphs /></ProtectedRoute>} />
           <Route path="senior/grade/8/introduction-to-graphs/introduction" element={<ProtectedRoute redirectTo="/login"><GraphsIntro5W1H /></ProtectedRoute>} />
